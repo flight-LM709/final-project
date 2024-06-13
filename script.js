e3e42f6dba54e3ababfd1f3a3161ccc9a550d892
flight-LM709 <150790738+flight-LM709@users.noreply.github.com>
2024-06-13 16:45:48 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+// Changing Jumbotron Image
+function changeImage(element){
+    element.setAttribute('src', 'assets/rowlet-jumbotron-2.jpg')
+}
+
+function changeImageBack(element){
+    element.setAttribute('src', 'assets/rowlet-jumbotron-1.jpg')
+}
+
+AOS.init()
