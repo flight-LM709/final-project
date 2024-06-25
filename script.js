3846264190b218a7f430b91c185650bff1855b5d
flight-LM709 <150790738+flight-LM709@users.noreply.github.com>
2024-06-25 17:35:24 +0700

    Add files via upload

--- script.js
+++ script.js
@@ -1,3 +1,31 @@
+// splash
+
+var splash = document.querySelector(".splash");
+if(splash) {
+  document.addEventListener('DOMContentLoaded', function(event) {
+    setTimeout(function() {
+      splash.style.display = 'none'
+    }, 3000);
+  })
+}
+
+var animate = document.querySelector('.astro2');
+
+if(animate) {
+  document.addEventListener('DOMContentLoaded', function(event){
+    setTimeout(function(){
+      animate.classList.add('animation')
+    }, 400);
+  })
+}
+
+var splashText = document.querySelector('.splash-text');
+
+if(splashText) {
+    setTimeout(function(){splashText.innerHTML = "Come in!"
+  }, 2000);
+}
+
 // Changing Jumbotron Image
 function changeImage(element){
     element.setAttribute('src', 'assets/rowlet-jumbotron-2.jpg')
